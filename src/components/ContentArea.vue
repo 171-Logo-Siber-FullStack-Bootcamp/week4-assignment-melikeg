b9f0ad666d19fe97baa2e647a87fd66615685e8d
Melike Görgülü <melikegorgulu.119@gmail.com>
2022-03-03 16:07:59 +0300

    first commit

---
+++ src/components/ContentArea.vue
@@ -0,0 +1,29 @@
+<template>
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+        <MainContent></MainContent>
+        <FooterArea></FooterArea>
+    </div>
+    <!-- End of Content Wrapper -->
+</template>
+
+<script>
+import MainContent from "./MainContent.vue"
+import FooterArea from "./FooterArea.vue"
+export default {
+    name: 'ContentArea',
+    components: {
+    MainContent,
+    FooterArea
+}
+}
+</script>
+
+<style>
+.flex-column {
+    flex-direction: column !important;
+}
+.d-flex {
+    display: flex !important;
+}
+</style>
