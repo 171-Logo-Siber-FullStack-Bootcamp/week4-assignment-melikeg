871cd124d168830a83d8d03f3d4ea99cabd4b440
Melike Görgülü <melikegorgulu.119@gmail.com>
2022-03-03 17:23:01 +0300

    created components PagesItem, UtilitiesItem and added to SidebarMenu.vue

--- src/components/SidebarMenu.vue
+++ src/components/SidebarMenu.vue
@@ -40,34 +40,7 @@
         </div>
     </li>
 
-    <!-- Nav Item - Utilities Collapse Menu -->
-    <li class="nav-item">
-        <a
-            class="nav-link collapsed"
-            href="#"
-            data-toggle="collapse"
-            data-target="#collapseUtilities"
-            aria-expanded="true"
-            aria-controls="collapseUtilities"
-        >
-            <i class="fas fa-fw fa-wrench"></i>
-            <span>Utilities</span>
-        </a>
-        <div
-            id="collapseUtilities"
-            class="collapse"
-            aria-labelledby="headingUtilities"
-            data-parent="#accordionSidebar"
-        >
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Utilities:</h6>
-                <a class="collapse-item" href="utilities-color.html">Colors</a>
-                <a class="collapse-item" href="utilities-border.html">Borders</a>
-                <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                <a class="collapse-item" href="utilities-other.html">Other</a>
-            </div>
-        </div>
-    </li>
+   <UtilitiesItem></UtilitiesItem>
 
     <!-- Divider -->
     <hr class="sidebar-divider" />
@@ -75,37 +48,7 @@
     <!-- Heading -->
     <div class="sidebar-heading">Addons</div>
 
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item active">
-        <a
-            class="nav-link"
-            href="#"
-            data-toggle="collapse"
-            data-target="#collapsePages"
-            aria-expanded="true"
-            aria-controls="collapsePages"
-        >
-            <i class="fas fa-fw fa-folder"></i>
-            <span>Pages</span>
-        </a>
-        <div
-            id="collapsePages"
-            class="collapse show"
-            aria-labelledby="headingPages"
-            data-parent="#accordionSidebar"
-        >
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Login Screens:</h6>
-                <a class="collapse-item" href="login.html">Login</a>
-                <a class="collapse-item" href="register.html">Register</a>
-                <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                <div class="collapse-divider"></div>
-                <h6 class="collapse-header">Other Pages:</h6>
-                <a class="collapse-item" href="404.html">404 Page</a>
-                <a class="collapse-item active" href="blank.html">Blank Page</a>
-            </div>
-        </div>
-    </li>
+    <PagesItem></PagesItem>
 
     <!-- Nav Item - Charts -->
     <li class="nav-item">
@@ -125,8 +68,11 @@
 </template>
 
 <script>
+import PagesItem from "./PagesItem.vue";
+import UtilitiesItem from "./UtilitiesItem.vue";
 export default {
     name: "SidebarMenu",
+    components: { PagesItem, UtilitiesItem }
 }
 </script>
 
