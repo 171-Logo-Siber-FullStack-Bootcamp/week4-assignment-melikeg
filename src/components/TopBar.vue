17085f728d44be0712222cc16c331311e089f619
Melike Görgülü <melikegorgulu.119@gmail.com>
2022-03-03 16:33:35 +0300

    SearchArea component included in TopBar

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -6,25 +6,7 @@
             <i class="fa fa-bars"></i>
         </button>
 
-        <!-- Topbar Search -->
-        <form
-            class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search"
-        >
-            <div class="input-group">
-                <input
-                    type="text"
-                    class="form-control bg-light border-0 small"
-                    placeholder="Search for..."
-                    aria-label="Search"
-                    aria-describedby="basic-addon2"
-                />
-                <div class="input-group-append">
-                    <button class="btn btn-primary" type="button">
-                        <i class="fas fa-search fa-sm"></i>
-                    </button>
-                </div>
-            </div>
-        </form>
+        <SearchArea></SearchArea>
 
         <!-- Topbar Navbar -->
         <ul class="navbar-nav ml-auto">
@@ -261,8 +243,10 @@
 </template>
 
 <script>
+import SearchArea from "./SearchArea.vue";
 export default {
-    name: 'TopBar',
+    name: "TopBar",
+    components: { SearchArea }
 }
 </script>
 
